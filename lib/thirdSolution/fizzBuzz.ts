f6fc9c62cadf452d87c9758dbb685be8ba641d1d
Francisco Mesa <franciscomesa@gmail.com>
2021-11-13 21:42:57 +0000

    ✨ add third solution as second solution

---
+++ lib/thirdSolution/fizzBuzz.ts
@@ -0,0 +1,33 @@
+export class fizzBuzz {
+
+  constructor(private iterations: number = 100) { }
+
+  generate(): string[] {
+    const result = []
+    for (let i = 1; i <= this.iterations; i++) {
+      result.push(fizzBuzz.calculateFizzBuzz(i))
+    }
+    return result
+  }
+
+  private static calculateFizzBuzz(value: number): string {
+    const fizz = 'Fizz'
+    const buzz = 'Buzz'
+    const fizzBuzz = fizz + buzz
+
+    const stringValue = String(value)
+
+    if (value % 3 === 0 && value % 5 === 0) {
+      return fizzBuzz
+    }
+    if (value % 3 === 0 || stringValue.indexOf('3') >= 0) {
+      return fizz
+    }
+    if (value % 5 === 0 || stringValue.indexOf('5') >= 0) {
+      // There are values with 5 digit that return fizz, pe: 51, 54
+      return buzz
+    }
+
+    return String(value)
+  }
+}
