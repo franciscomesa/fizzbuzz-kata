0c650fa9dfe2c11f5b59639fde88f9e014f8fe3a
Francisco Mesa <franciscomesa@gmail.com>
2021-11-13 19:32:54 +0000

    ✨ add bin to run kata

--- lib/firstSolution/fizzBuzzFunction.ts
+++ lib/firstSolution/fizzBuzzFunction.ts
@@ -4,7 +4,6 @@ export function calculateFizzBuzz(value: number): string {
   const fizzBuzz = fizz + buzz
 
   if (value % 3 === 0 && value % 5 === 0) {
-    console.log('what!', fizzBuzz)
     return fizzBuzz
   }
   if (value % 3 === 0) {
