a045389214f995b3d18f9c0520147162559fbdc5
Francisco Mesa <franciscomesa@gmail.com>
2021-11-13 19:22:59 +0000

    ✅ add FizzBuzz message to numbers divisible by 3 and 5

--- lib/firstSolution/fizzBuzzFunction.ts
+++ lib/firstSolution/fizzBuzzFunction.ts
@@ -1,7 +1,12 @@
-export function fizzBuzz(value: number): string {
+export function calculateFizzBuzz(value: number): string {
   const fizz = 'Fizz'
   const buzz = 'Buzz'
+  const fizzBuzz = fizz + buzz
 
+  if (value % 3 === 0 && value % 5 === 0) {
+    console.log('what!', fizzBuzz)
+    return fizzBuzz
+  }
   if (value % 3 === 0) {
     return fizz
   }
--- test/fizz-buzz.test.ts
+++ test/fizz-buzz.test.ts
@@ -1,14 +1,15 @@
-import { fizzBuzz } from '../lib/firstSolution/fizzBuzzFunction'
+import { calculateFizzBuzz } from '../lib/firstSolution/fizzBuzzFunction'
 import { expectNumber } from './expectNumber'
 
 describe('Fizz buzz algorithm should', () => {
   const fizzValue = 'Fizz'
   const buzzValue = 'Buzz'
+  const fizzbuzzValue = 'FizzBuzz'
 
   it('1 value return 1', () => {
     const value = 1
 
-    const fizzBuzzValue = fizzBuzz(value)
+    const fizzBuzzValue = calculateFizzBuzz(value)
 
     expectNumber(value).toBe(fizzBuzzValue)
   })
@@ -16,7 +17,7 @@ describe('Fizz buzz algorithm should', () => {
   it('2 value return 2', () => {
     const value = 2
 
-    const fizzBuzzValue = fizzBuzz(value)
+    const fizzBuzzValue = calculateFizzBuzz(value)
 
     expectNumber(value).toBe(fizzBuzzValue)
   })
@@ -24,7 +25,7 @@ describe('Fizz buzz algorithm should', () => {
   it('3 value return Fizz', () => {
     const value = 3
 
-    const fizzBuzzValue = fizzBuzz(value)
+    const fizzBuzzValue = calculateFizzBuzz(value)
 
     expect(fizzBuzzValue).not.toBe(value)
     expectNumber(fizzValue).toBe(fizzBuzzValue)
@@ -33,7 +34,7 @@ describe('Fizz buzz algorithm should', () => {
   it('4 value return 4', () => {
     const value = 3
 
-    const fizzBuzzValue = fizzBuzz(value)
+    const fizzBuzzValue = calculateFizzBuzz(value)
 
     expect(fizzBuzzValue).not.toBe(value)
     expectNumber(fizzValue).toBe(fizzBuzzValue)
@@ -42,7 +43,7 @@ describe('Fizz buzz algorithm should', () => {
   it('5 value return Buzz', () => {
     const value = 5
 
-    const fizzBuzzValue = fizzBuzz(value)
+    const fizzBuzzValue = calculateFizzBuzz(value)
 
     expect(fizzBuzzValue).not.toBe(value)
     expectNumber(buzzValue).toBe(fizzBuzzValue)
@@ -51,7 +52,7 @@ describe('Fizz buzz algorithm should', () => {
   it('6 value return Fizz', () => {
     const value = 6
 
-    const fizzBuzzValue = fizzBuzz(value)
+    const fizzBuzzValue = calculateFizzBuzz(value)
 
     expect(fizzBuzzValue).not.toBe(value)
     expectNumber(fizzValue).toBe(fizzBuzzValue)
@@ -60,11 +61,19 @@ describe('Fizz buzz algorithm should', () => {
   it('10 value return Buzz', () => {
     const value = 10
 
-    const fizzBuzzValue = fizzBuzz(value)
+    const fizzBuzzValue = calculateFizzBuzz(value)
 
     expect(fizzBuzzValue).not.toBe(value)
     expectNumber(buzzValue).toBe(fizzBuzzValue)
   })
 
+  it('15 value return FizzBuzz', () => {
+    const value = 15
+
+    const result = calculateFizzBuzz(value)
+
+    expect(result).not.toBe(value)
+    expectNumber(result).toBe(fizzbuzzValue)
+  })
 
 })
