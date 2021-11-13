d30d14c96560625e24828a2348fe5bc7ed1817e3
Francisco Mesa <franciscomesa@gmail.com>
2021-11-13 18:30:26 +0000

    ✅ number 2 return 2

--- test/fizz-buzz.test.ts
+++ test/fizz-buzz.test.ts
@@ -1,8 +1,16 @@
-import { fizzBuzz } from '../lib/firstSolution/fizzBuzz'
+import { fizzBuzz } from '../lib/firstSolution/fizzBuzzFunction'
 
 describe('Fizz buzz algorithm should', () => {
   it('1 value return 1', () => {
     const value = 1
+
+    const fizzBuzzValue = fizzBuzz(value)
+
+    expect(fizzBuzzValue).toBe(value)
+  })
+
+  it('2 value return 2', () => {
+    const value = 2
 
     const fizzBuzzValue = fizzBuzz(value)
 
