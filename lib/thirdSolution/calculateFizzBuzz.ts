2a05b152ceb05648c1319cd5d9f5ebfff4bd46fc
Francisco Mesa <franciscomesa@gmail.com>
2021-11-14 00:59:08 +0000

    ♻️ rename method

--- lib/thirdSolution/calculateFizzBuzz.ts
+++ lib/thirdSolution/calculateFizzBuzz.ts
@@ -4,7 +4,7 @@ import { CalculateRule } from "."
 export class calculateFizzBuzz {
   constructor(private readonly rules: CalculateRule[] = []) {}
 
-  generate(value: number): string {
+  parse(value: number): string {
     for (const rule of this.rules) {
       if (rule.matches(value)) {
         return rule.parse()
