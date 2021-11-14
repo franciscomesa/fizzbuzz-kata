5d0a73ed2a0907fc1b139c71d6006f4469d3bc81
Francisco Mesa <franciscomesa@gmail.com>
2021-11-14 00:06:34 +0000

    ♻️ set rules at inicialization

--- lib/thirdSolution/calculateFizzBuzz.ts
+++ lib/thirdSolution/calculateFizzBuzz.ts
@@ -2,21 +2,16 @@ import { CalculateRule } from "."
 
 
 export class calculateFizzBuzz {
-  private rules: CalculateRule[] = []
+  constructor(private readonly rules: CalculateRule[] = []) {}
 
-  constructor(private readonly valueNumber: number) {  }
-
-  addRule(rule: CalculateRule): void {
-    this.rules.push(rule)
-  }
-  get value(): string {
+  generate(value: number): string {
     for (const rule of this.rules) {
-      if (rule.matches(this.valueNumber)) {
+      if (rule.matches(value)) {
         return rule.parse()
       }
     }
 
-    return String(this.valueNumber)
+    return String(value)
     }
 }
 
--- lib/thirdSolution/fizzBuzz.ts
+++ lib/thirdSolution/fizzBuzz.ts
@@ -2,20 +2,18 @@ import { buzzRule, fizzBuzzRule, fizzRule } from "."
 import { calculateFizzBuzz } from "./calculateFizzBuzz"
 
 export class fizzBuzz {
-  private static fizz = new fizzRule()
-  private static buzz = new buzzRule()
-  private static fizzBuzz = new fizzBuzzRule()
+  private static calculate = new calculateFizzBuzz([
+    new fizzBuzzRule(),
+    new fizzRule(),
+    new buzzRule()
+    ])
 
   constructor(private iterations: number = 100) { }
 
   generate(): string[] {
     const result = []
     for (let i = 1; i <= this.iterations; i++) {
-      const calculate = new calculateFizzBuzz(i)
-      calculate.addRule(fizzBuzz.fizzBuzz)
-      calculate.addRule(fizzBuzz.fizz)
-      calculate.addRule(fizzBuzz.buzz)
-      result.push(calculate.value)
+      result.push(fizzBuzz.calculate.generate(i))
     }
     return result
   }
